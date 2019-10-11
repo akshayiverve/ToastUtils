39c1a4b606887952f7c0e924c003f5449cf0528a
akshayiverve <akshay.more@ifuturz.com>
2019-10-11 18:56:03 +0530

    First Commit
    First Commit

--- ToastUtilsLib/src/main/java/com/toastutilslib/ToastUtils.java
+++ ToastUtilsLib/src/main/java/com/toastutilslib/ToastUtils.java
@@ -8,5 +8,7 @@ public class ToastUtils {
     public static void showToast(Context context, String Message) {
         Toast.makeText(context, Message, Toast.LENGTH_SHORT).show();
     }
-    public static void getHello(){}
+    public static void getHello(){
+        
+    }
 }
