73c6fb1b834f4ed211f3f10d9adc876c582bae44
akshayiverve <akshay.more@ifuturz.com>
2019-10-11 18:54:59 +0530

    function add

--- ToastUtilsLib/src/main/java/com/toastutilslib/ToastUtils.java
+++ ToastUtilsLib/src/main/java/com/toastutilslib/ToastUtils.java
@@ -8,4 +8,5 @@ public class ToastUtils {
     public static void showToast(Context context, String Message) {
         Toast.makeText(context, Message, Toast.LENGTH_SHORT).show();
     }
+    public static void getHello(){}
 }
