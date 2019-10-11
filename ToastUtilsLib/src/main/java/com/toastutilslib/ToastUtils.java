cbaf36c7dd78acb4c85fc01e04e5fc3d0a1cc51c
akshayiverve <akshay.more@ifuturz.com>
2019-10-11 18:04:58 +0530

    Initial commit

---
+++ ToastUtilsLib/src/main/java/com/toastutilslib/ToastUtils.java
@@ -0,0 +1,11 @@
+package com.toastutilslib;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToastUtils {
+
+    public static void showToast(Context context, String Message) {
+        Toast.makeText(context, Message, Toast.LENGTH_SHORT).show();
+    }
+}
